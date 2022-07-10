81b99e6ce2df5796548072b97880f5a47487bcf6
Kevin Blin <kblin@outlook.com>
2022-07-10 12:43:30 +0200

    Release 0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="vat-pytorch",
     packages=find_packages(exclude=[]),
-    version="0.0.5",
+    version="0.0.6",
     license="MIT",
     description="Virtual Adversarial Training - Pytorch",
     long_description_content_type="text/markdown",
