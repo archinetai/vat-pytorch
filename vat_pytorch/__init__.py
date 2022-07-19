e648ab460e1f8c4a3074958d3f43779148df74cc
Flavio Schneider <flavio.schneider.97@gmail.com>
2022-07-19 22:27:33 +0200

    fix: wrong module import

--- vat_pytorch/__init__.py
+++ vat_pytorch/__init__.py
@@ -2,4 +2,4 @@ from .smart import SMARTLoss
 from .alice import ALICELoss
 from .alicepp import ALICEPPLoss, ALICEPPModule
 from .utils import kl_loss, sym_kl_loss, js_loss, inf_norm, default
-from .modules import *
+from .models import *
